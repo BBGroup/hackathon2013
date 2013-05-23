92756dbb6f59746d571d6910fdb07f6bd0075704
Luca <Luca@10.0.0.241>
2013-05-23 16:14:11 +0200

    set

---
+++ Hackathon2013/src/it/univpm/hackathon2013/Settings.java
@@ -0,0 +1,18 @@
+package it.univpm.hackathon2013;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Settings extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+	}
+
+
+
+}
