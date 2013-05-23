68a3648a384fb7ef02617ec02c216347fd936115
Luca <Luca@10.0.0.241>
2013-05-23 12:53:50 +0200

    main 12.53

--- Hackathon2013/src/it/univpm/hackathon2013/MainActivity.java
+++ Hackathon2013/src/it/univpm/hackathon2013/MainActivity.java
@@ -2,13 +2,20 @@ package it.univpm.hackathon2013;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	
+	
+	String misa="misaBasin";
+	String cesano="cesanoBasin";
+	String esino="esinoBasin";
+	String metauro="metauroBasin";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,44 +23,41 @@ public class MainActivity extends Activity {
         
         Button misaButton = (Button) findViewById(R.id.misabutton);
         misaButton.setOnClickListener(new OnClickListener(){
-        	
         	@Override
         	public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+				Intent intent = new Intent(MainActivity.this , BasinActivity.class);
+                intent.putExtra("misa", misa);
+				startActivity( intent  );
 			}
         });
         
         Button metauroButton = (Button) findViewById(R.id.metaurobutton);
         metauroButton.setOnClickListener(new OnClickListener() {
-        	
-			
-			@Override
+        	@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+				Intent intent = new Intent(MainActivity.this , BasinActivity.class);
+                intent.putExtra("metauro", metauro);
+				startActivity( intent  );
 			}
 		});
         
         Button esinoButton = (Button) findViewById(R.id.metaurobutton);
         esinoButton.setOnClickListener(new OnClickListener() {
-        	
-			
-			@Override
+        	@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+				Intent intent = new Intent(MainActivity.this , BasinActivity.class);
+                intent.putExtra("esino", esino);
+				startActivity( intent  );
 			}
 		});
         
         Button cesanoButton = (Button) findViewById(R.id.metaurobutton);
         cesanoButton.setOnClickListener(new OnClickListener() {
-        	
-			
-			@Override
+        	@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+        		Intent intent = new Intent(MainActivity.this , BasinActivity.class);
+                intent.putExtra("cesano", cesano);
+				startActivity( intent  );
 			}
 		});
         
