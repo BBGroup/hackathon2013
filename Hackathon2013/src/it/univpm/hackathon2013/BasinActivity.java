22a546d3bc5bfc5a35a733d28275552396689e1a
Luca <Luca@10.0.0.241>
2013-05-23 14:37:06 +0200

    basinact1437

--- Hackathon2013/src/it/univpm/hackathon2013/BasinActivity.java
+++ Hackathon2013/src/it/univpm/hackathon2013/BasinActivity.java
@@ -1,19 +1,110 @@
 package it.univpm.hackathon2013;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 
-public class BasinActivity extends ListActivity{
+public class BasinActivity extends Activity{
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		this.setContentView(it.univpm.hackathon2013.R.layout.basin_activity);
-		
+		ListView lv=(ListView)this.findViewById(R.id.lista);
 		ListAdapter adapter=createAdapter();
-		setListAdapter(adapter);
+		lv.setAdapter(adapter);
+		
+		SqliteManager db=new SqliteManager(getApplicationContext());
+        db.open();  //apriamo il db
+       
+        if(db.fetchProducts().getCount()==0){
+        	db.inserisciBacino(0, "Misa");
+        	db.inserisciBacino(1, "Cesano");
+        	db.inserisciBacino(2, "Metauro");
+        	db.inserisciBacino(3, "Esino");
+        	
+        	db.inserisciStazione("Misa","026",0,0);
+        	db.inserisciStazione("Nevola","120",0,0);
+        	db.inserisciStazione("Serra de' Conti","003",0,0);
+        	db.inserisciStazione("Montecarotto","119",1,0);
+        	db.inserisciStazione("Arcevia","123",1,0);
+        	
+        	db.inserisciStazione("Cesano Foce","121",0,1);
+        	db.inserisciStazione("Cesano","018",0,1);
+        	db.inserisciStazione("Pergola","108",0,1);
+        	db.inserisciStazione("Pergola","108",1,1);
+        	
+        	db.inserisciStazione("Metauro Foce","152",0,2);
+        	db.inserisciStazione("Metauro","017",0,2);
+        	db.inserisciStazione("Ghilardino (Tarugo)","105",0,2);
+        	db.inserisciStazione("Fossombrone (Metauro)","151",0,2);
+        	db.inserisciStazione("Acqualagna (Candigliano)","106",0,2);
+        	db.inserisciStazione("Biscubio","107",0,2);
+        	db.inserisciStazione("Sant'Angelo in Vado","101",0,2);
+        	db.inserisciStazione("Metauro Foce","152",1,2);
+        	db.inserisciStazione("Metauro","017",1,2);
+        	db.inserisciStazione("Fossombrone (Metauro)","151",1,2);
+        	db.inserisciStazione("Biscubio","107",1,2);
+        	db.inserisciStazione("Sant'Angelo in Vado","101",1,2);
+        	
+        	db.inserisciStazione("Monte San Vito (Traponzio)","122",0,3);
+        	db.inserisciStazione("Chiaravalle","507",0,3);
+        	db.inserisciStazione("Moie","506",0,3);
+        	db.inserisciStazione("Gola della Rossa","006",0,3);
+        	db.inserisciStazione("Camponocecchio","005",0,3);
+        	db.inserisciStazione("San Vittore","004",0,3);
+        	db.inserisciStazione("Sassoferrato","505",0,3);
+        	db.inserisciStazione("Fabriano","002",0,3);
+        	db.inserisciStazione("Fabriano2","028",0,3);
+        	db.inserisciStazione("Esanatoglia","001",0,3);
+        	db.inserisciStazione("Agugliano","111",1,3);
+        	db.inserisciStazione("Jesi","110",1,3);
+        	db.inserisciStazione("Montecarotto","119",1,3);
+        	db.inserisciStazione("Cupramontana","118",1,3);
+        	db.inserisciStazione("San Giovanni","504",1,3);
+        	db.inserisciStazione("Campodiegoli","502",1,3);
+        	db.inserisciStazione("Esanatoglia 2","503",1,3);
+        	
+        }
+       
+       
+
+        Cursor c=db.fetchProducts(); // query
+        startManagingCursor(c);
+
+
+       
+       
+        SimpleCursorAdapter adapter=new SimpleCursorAdapter( //semplice adapter per i cursor
+                        this,
+                        R.layout.product, //il layout di ogni riga/prodotto
+                        c,
+                        new String[]{MyDatabase.ProductsMetaData.PRODUCT_NAME_KEY,MyDatabase.ProductsMetaData.PRODUCT_PRICE_KEY},//questi colonne
+                        new int[]{R.id.nameTv,R.id.priceTv});//in queste views
+       
+       
+
+       
+       
+        productsLv.setAdapter(adapter); //la listview ha questo adapter
+       
+       
+        //qui vediamo invece come reperire i dati e usarli, in questo caso li stampiamo in una textview
+       
+        int nameCol=c.getColumnIndex(MyDatabase.ProductsMetaData.PRODUCT_NAME_KEY);  //indici delle colonne
+        int priceCol=c.getColumnIndex(MyDatabase.ProductsMetaData.PRODUCT_PRICE_KEY);      
+       
+        if(c.moveToFirst()){  //se va alla prima entry, il cursore non è vuoto
+                do {
+                               
+                        productsTv.append("Product Name:"+c.getString(nameCol)+", Price:"+c.getInt(priceCol)+"\n"); //estrazione dei dati dalla entry del cursor
+                                       
+                        } while (c.moveToNext());//iteriamo al prossimo elemento
+        }
+       
+        db.close();
 		
 	}
 	
