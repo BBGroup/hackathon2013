95104d1c4f206a72f560c2371a3d8fa1f496d6a9
Luca <Luca@10.0.0.241>
2013-05-23 12:01:01 +0200

    12.00

--- Hackathon2013/src/it/univpm/hackathon2013/MainActivity.java
+++ Hackathon2013/src/it/univpm/hackathon2013/MainActivity.java
@@ -3,6 +3,7 @@ package it.univpm.hackathon2013;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,9 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //modifica2
-        //modifica matteo
-        //modifica enry
+        
+        Button misaButton = (Button) findViewById(R.id.misabutton);
+        
     }
 
 
