cfc87ad1303d78f6d4bbcb0007e16a3d980f0772
Luca <Luca@10.0.0.241>
2013-05-24 12:10:05 +0200

    12000

--- Hackathon2013/gen/it/univpm/hackathon2013/R.java
+++ Hackathon2013/gen/it/univpm/hackathon2013/R.java
@@ -38,18 +38,19 @@ public final class R {
         public static final int marchemis=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
-        public static final int data=0x7f090007;
+        public static final int action_settings=0x7f09000c;
+        public static final int data=0x7f090008;
         public static final int graphView=0x7f090000;
-        public static final int incremento=0x7f09000a;
-        public static final int info=0x7f090005;
-        public static final int mainLayout=0x7f090004;
+        public static final int incremento=0x7f09000b;
+        public static final int info=0x7f090006;
+        public static final int mainLayout=0x7f090005;
         public static final int marcheMap=0x7f090003;
-        public static final int nome=0x7f090006;
-        public static final int ora=0x7f090008;
+        public static final int nome=0x7f090007;
+        public static final int ora=0x7f090009;
+        public static final int scrollView1=0x7f090004;
         public static final int selectedArea=0x7f090002;
         public static final int tv=0x7f090001;
-        public static final int valore=0x7f090009;
+        public static final int valore=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_chart=0x7f030000;
--- Hackathon2013/src/it/univpm/hackathon2013/BasinActivity.java
+++ Hackathon2013/src/it/univpm/hackathon2013/BasinActivity.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.widget.ImageButton;
@@ -14,6 +20,9 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class BasinActivity extends Activity {
+	
+	private static final int SIMPLE_NOTIFICATION_ID = 1;
+	  NotificationManager mNotificationManager;
 
 	// public SqliteManager db=null;
 	// public DataSource datasource;
@@ -24,6 +33,9 @@ public class BasinActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(it.univpm.hackathon2013.R.layout.basin_activity);
+		
+		mNotificationManager = (NotificationManager) getSystemService(this.NOTIFICATION_SERVICE);
+		
 		//lv = (ListView) this.findViewById(R.id.list);
 		//TextView productsTv = (TextView) findViewById(R.id.product);
 //		MyDatabase db = new MyDatabase(getApplicationContext());
@@ -59,14 +71,23 @@ public class BasinActivity extends Activity {
 	}
 	public void refreshTabella(ArrayList<ArrayList<String>> tabella){
 		Iterator<ArrayList<String>> iter=tabella.iterator();
+		int i=0;
 		while(iter.hasNext()){
-			refreshRiga(iter.next());	
+			int color=Color.WHITE;
+			if(i%2==0){
+				color=Color.GRAY;
+			}else{
+				color=Color.WHITE;
+			}
+			refreshRiga(iter.next(),color);
+			i++;
 		}
 	}
-	public void refreshRiga(ArrayList<String> list){
+	public void refreshRiga(ArrayList<String> list,int color){
 		LinearLayout layout=(LinearLayout)this.findViewById(R.id.mainLayout);
 		LinearLayout row=new LinearLayout(this);
 		row.setOrientation(LinearLayout.VERTICAL);
+		row.setBackgroundColor(color);
 //		Iterator<String> iter=list.iterator();
 //		while(iter.hasNext()){
 		if(list.get(3).equals("0")){
@@ -85,9 +106,47 @@ public class BasinActivity extends Activity {
 
 			TextView cell3=new TextView(this);
 			cell3.setText(list.get(1));
+			if(Double.parseDouble(list.get(1))>1){
+				cell3.setBackgroundColor(Color.RED);
+			}else{
+				cell3.setBackgroundColor(Color.TRANSPARENT);
+			}
 			row.addView(cell3);
+			
 		ImageButton graphButton = new ImageButton (this);
 		graphButton.setImageResource(R.drawable.graph);
 		layout.addView(row);
 	}
+	public void sendSimpleNotification(String message) {
+    	mNotificationManager = (NotificationManager) getSystemService(this.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(
+                        BasinActivity.this);
+
+        // Titolo e testo della notifica
+        notificationBuilder.setContentTitle("Calamity Watcher Notification");
+        notificationBuilder.setContentText(message);
+
+        // Testo che compare nella barra di stato non appena compare la notifica
+        notificationBuilder.setTicker("Allerta!");
+
+        // Data e ora della notifica
+        notificationBuilder.setWhen(System.currentTimeMillis());
+
+        // Icona della notifica
+        notificationBuilder.setSmallIcon(R.drawable.icona4);
+
+        // Creiamo il pending intent che verr� lanciato quando la notifica
+        // viene premuta
+        Intent notificationIntent = new Intent(this,  MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                        notificationIntent, 0);
+        
+        notificationBuilder.setContentIntent(contentIntent);
+
+        // Impostiamo il suono, le luci e la vibrazione di default
+        notificationBuilder.setDefaults(Notification.DEFAULT_SOUND
+                        | Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE);
+
+        mNotificationManager.notify(SIMPLE_NOTIFICATION_ID,                        notificationBuilder.build());
+}
 }
